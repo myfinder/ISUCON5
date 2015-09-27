bd291d87a0e815142b22cf382fcb59234dee1e8f
Kan Fushihara <kan.fushihara@gmail.com>
2015-09-27 15:06:41 +0900

    access.logの解析スクリプト

---
+++ benchi-logs/parse.pl
@@ -0,0 +1,15 @@
+use Text::LTSV;
+use Data::Dumper;
+
+my $ltsv = Text::LTSV->new;
+my %cnt;
+
+while(my $line = <STDIN>) {
+    my $row = $ltsv->parse_line($line);
+    $cnt{$row->{uri}} //= 0;
+    $cnt{$row->{uri}}++;
+}
+
+for my $uri (keys %cnt) {
+    print $uri . "\t" . $cnt{$uri} . "\n";
+}
